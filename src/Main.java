9ca28a43e364e471e87d43fade61267057aaa49b
Ryuichi Nakahodo <e235755@ie.u-ryukyu.ac.jp>
2023-12-05 18:37:43 +0900

    ステップ2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+        try {
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(e.getMessage());
+        }
     }
 }
