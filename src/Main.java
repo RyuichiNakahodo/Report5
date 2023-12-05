4356c5dced31541d827fbc0d6cbdd7f53e4c4beb
Ryuichi Nakahodo <e235755@ie.u-ryukyu.ac.jp>
2023-12-05 20:15:54 +0900

    ステップ3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        String str = null;
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生した");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        
+
     }
 }
